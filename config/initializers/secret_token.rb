8c93b0402041eae84fb719ea0349f7db12090c9f
Ryan Schmukler <rschmukler@gmail.com>
2012-04-18 23:22:38 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Voter::Application.config.secret_token = 'b1f8e550f17765a58e0d6e30dcc451ebf0bcf5d1fd675ed3578d5f8ba163944acfbc36e896e5c7da9c840db7f1df299fbeea50483fd35a4d0df8f1055b07ff42'
