856def5bbf1370a95cc2472d479ff116f067beda
Ryan Schmukler <rschmukler@gmail.com>
2012-04-19 01:00:57 -0500

    Added talk validations and new/creation actions

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -2,4 +2,20 @@ class TalksController < ApplicationController
   def index
     @talks = Talk.all
   end
+
+  def new
+    @talk = Talk.new
+  end
+
+  def create
+    @talk = Talk.new(params[:talk])
+    if @talk.save
+      flash[:notice] = "Successfully added talk!"
+      redirect_to talks_path
+    else
+      flash[:alert] = "Invalid talk. Please check and try again!"
+      render 'new'
+    end
+  end
+
 end
